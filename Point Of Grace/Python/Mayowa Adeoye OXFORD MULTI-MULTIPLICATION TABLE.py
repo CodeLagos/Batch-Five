121e13e5b41b6ae4d77d53685b980ae3ee962a12
delekstech <43611808+delekstech@users.noreply.github.com>
2018-12-12 22:55:39 +0100

    Add files via upload

---
+++ Point Of Grace/Python/Mayowa Adeoye OXFORD MULTI-MULTIPLICATION TABLE.py
@@ -0,0 +1,29 @@
+print("welcome to the ADE-MAYOWA OXFORD MULTI-MULTIPLICATION TABLE")
+print("Please enter the number you want to view the multiplication table")
+print("Please Enter Whole Number")
+print(" ")
+
+value=int(input("Please enter a number: "))
+for i in range(1,21,1):
+    print(value,"*",i,"=",value*i)
+
+value=int(input("Please enter a number: "))
+for i in range(1,21,1):
+    print(value,"*",i,"=",value*i)
+
+value=int(input("Please enter a number: "))
+for i in range(1,21,1):
+    print(value,"*",i,"=",value*i)
+
+value=int(input("Please enter a number: "))
+for i in range(1,21,1):
+    print(value,"*",i,"=",value*i)
+
+value=int(input("Please enter a number: "))
+for i in range(1,21,1):
+    print(value,"*",i,"=",value*i)
+
+value=int(input("Please enter a number: "))
+for i in range(1,21,1):
+    print(value,"*",i,"=",value*i)
+print ("THE END")
