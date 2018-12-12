baecc6324aff71fea7c514b38dcd31a3764dca0e
surepeps <31157177+surepeps@users.noreply.github.com>
2018-12-12 18:38:52 +0100

    Create hello.py

---
+++ TechQuest/Python/hello.py
@@ -0,0 +1,8 @@
+##############################Welcome page for CODELAGOS TECHQUEST CENTER PYTHON PROJECT REPOSITORY FOLDER###############
+print("WELCOME TO TECHQUEST CENTER PROJECT FOLDER LET'S CONFIRM IF YOU BELONG TO TECHQUEST")
+name = input("Enter Your Surname:-")
+center = input("Enter Your Center Name:-")
+if (center == "TechQuest") or (center == "TECHQUEST") or (center == "Tech Quest") or (center == "tech quest") or (center == "techquest"):
+  print("Hello Mr/Mrs/Dr/Prof", name, ", Mr Hassan Tijani Welcome you to TechQuest")
+else:
+print("Wrong Center Please Find your Center")
