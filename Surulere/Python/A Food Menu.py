20a32b665d7597144d594ab125551096fc8769ab
MichaelBakare <45696155+MichaelBakare@users.noreply.github.com>
2018-12-12 23:58:51 +0100

    Create  A Food Menu.py

---
+++ Surulere/Python/A Food Menu.py
@@ -0,0 +1,44 @@
+#A food Menu by Michael Bakare
+#CodeLagos Batch 5 Surulere Aguda Center
+
+#Display the purpose of the code
+print("CodeLagos Project of a food Menu")
+
+#Display the instruction
+print("Welcome to Michael Bakare canteen")
+
+
+    
+#Dispaly the options
+menu=int(input("Please select your order \n1)Rice and Chicken \n2)Amala and fish \n3)Chicken and chips \n4)Fufu and beef \n5)Dodo and egg\n"))
+
+#1 == "Rice and Chicken"
+#2 == "Amala and fish"
+#3 == "Chicken and chips"
+#4 == "Fufu and beef"
+#5 == "Dodo and egg"
+
+
+#Print out the customer's choice
+if (menu == 1):
+    print("Your order is Rice and Chicken")
+    
+elif(menu == 2):
+    print("Your order is Amala and fish")
+    
+elif(menu == 3):
+    print("Your order is Chicken and Chips")
+
+elif(menu == 4):
+     print("Your order is Fufu and beef")
+    
+elif(menu == 5):
+    print("Your order is Dodo and egg")
+    
+else:
+    print("Your order is Invalid order")
+
+print("Thank you for patronizing us, Hope to see you soon.")
+
+
+
