157ab73a48e367dfc98e5dfa86e3de04517fc665
fixani <45657451+fixani@users.noreply.github.com>
2018-12-14 09:12:20 +0100

    By Sola  Dele

---
+++ Surulere/python/project.py
@@ -0,0 +1,15 @@
+print("welcome to olusola program")
+name=input("what is your name:")
+olusola=name
+print(name)
+print("this is a progam to determine if a number is even/odd")
+
+print("this program will only work for whole numbers")
+
+i = 1
+while 1<6:
+    number=int(input("pls enter an even/odd number:"))
+    if (number %2 == 0):
+        print(number,"is even")
+    else:
+            print(number,"is odd")
