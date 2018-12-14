ad62ff5f457c1a18e1f67e5333ce1789f71bd111
delekstech <43611808+delekstech@users.noreply.github.com>
2018-12-14 08:34:58 +0100

    Add files via upload

---
+++ Point Of Grace/Python/olagunju blessing.py
@@ -0,0 +1,317 @@
+
+enter=input("enter start to begin: ")
+
+print("WORD HUNT")
+print("MAKE WORDS FOUND IN THE DICTIONARY FROM THE DISPLAYED WORD")
+print("ANY INTENTIONALLY REPEATED WORD WILL CAUSE YOU TO LOOSE THE GAME")
+print("\n \nHUNTERS LETS START HUNTING")
+print("type give up if you're out of words")
+score=0
+word=print("CPUDIE")
+answer=("CUPID \nDIE \nPIE \nDUPE \nDIP \nCUP \nEPIC")
+print("your word is")
+print(word)
+first =input("enter your word\n")
+if (first.lower()=="cupid"):
+    print("good job")
+    score=(score+1)
+    print(score)
+elif (first.lower()=="die"):
+    print("good job")
+    score=(score+1)
+    print(score)
+elif (first.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (first.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (first.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (first.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (first.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print(score)
+else:
+    print("wrong word")
+    score==score
+    print(answer)
+    print(score)
+   
+    
+    
+
+second=input("enter your word \n")
+if (second==first):
+    print("repeated word,game over")
+    exit()
+if (second.lower()=="cupid"):
+     print("goodjob")
+     score=score+1
+     print(score)
+elif (second.lower()=="die"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (second.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (second.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (second.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (second.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (second.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print(score)
+else:
+    print("wrong word")
+    score==score
+    print(answer)
+    print(score)
+    
+
+third=input("enter your word \n")
+if (third==second or third==first):
+    print("repeated word,game over")
+    exit()
+if (third.lower()=="cupid"):
+     print("good job")
+     score=score+1
+     print(score)
+elif (third.lower()=="die"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (third.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (third.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (third.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (third.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (third.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print(score)
+    score==score+1
+    print("SCORE IS=")
+    print(score)
+    print ("nice work")
+    print(score)
+else:
+    print("wrong word")
+    score==score
+    print(answer)
+    print(score)
+    
+
+fourth=input("enter your word \n")
+if (fourth==third or fourth==second or fourth==first):
+    print("repeated word,game over")
+    exit()
+if (fourth.lower()=="cupid"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="die"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("nice work")
+    print(score)
+else:
+    print("wrong word")
+    score==score
+    print(answer)
+    print(score)
+    
+
+
+
+
+fifth=input("enter your word \n")
+if (fifth==fourth or fifth==third or fifth==second or fifth==first):
+    print("repeated word,game over")
+    exit()
+if (fourth.lower()=="cupid"):
+     print("good job")
+     score=score+1
+     print(score)
+elif (fourth.lower()=="die"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("nice work")
+    print(score)
+else:
+    print("wrong word")
+    score==score
+    print(answer)
+    print(score)
+    main()
+
+   
+
+
+sixth=input("enter your word \n")
+if (sixth== fifth or sixth==fourth or sixth==third or sixth==second or sixth==first):
+    print("repeated word,game over")
+    exit()
+if (fourth.lower()=="cupid"):
+     print("good job")
+     score=score+1
+     print(score)
+elif (fourth.lower()=="die"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+elif (fourth.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print ("nice work")
+    print(score)
+else:
+    print("wrong word")
+    score==score
+    print(answer)
+    print(score)
+
+
+
+
+
+seventh=input("enter your word \n")
+if (seventh==sixth or seventh==fifth or seventh==fourth or seventh==third or seventh==second or seventh==first):
+    print("repeated word,game over")
+    exit()
+if (fourth.lower()=="cupid"):
+     print("good job")
+     score=score+1
+     print(score)
+     print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+elif (fourth.lower()=="die"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+elif (fourth.lower()=="pie"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+elif (fourth.lower()=="dupe"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+elif (fourth.lower()=="dip"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+elif (fourth.lower()=="cup"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+elif (fourth.lower()=="epic"):
+    print("good job")
+    score=score+1
+    print(score)
+    print ("nice work")
+    print(score)
+    print ("CONGRATULATIONS FELLOW HUNTER, FANTASTIC HUNTING")
+else:
+    print("wrong word")
+    score=score
+    print(answer)
+    print(score)
+
+
+
+
+
+
