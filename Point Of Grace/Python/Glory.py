71891069602189fbb376245c6f5abc5f7de465fe
delekstech <43611808+delekstech@users.noreply.github.com>
2018-12-15 22:50:08 +0100

    Add files via upload

---
+++ Point Of Grace/Python/Glory.py
@@ -0,0 +1,75 @@
+#Project Work Done By: Buraimoh Glory
+
+
+#print out the purpose of the program
+print("This program will teach you how to prepare some selected Food")
+print("----------------------------------------------------------------------")
+#print out the instructions
+print("NOTICE: Please Select a number corresponding to your preferred Menu")
+print("                                                           ")
+#display the menus
+print("======================================================================")
+print("Option 1: Fried Rice and Chicken")
+print("Option 2: Coconut Rice and Beef")
+print("Option 3: Amala and Egusi Soup With Fish")
+print("Option 4: Ofada Rice with Fish and Beef Sauce")
+print("======================================================================")
+
+print("                            ")
+#ask the user to make a choice
+
+menu=int(input("Please Select an Option \n1) Option1\n2) Option2\n3) Option3\n4) Option4\nEnter Your Option Number Here: "))
+
+#print out the user's choice
+if (menu==1):
+    print("Option 1")
+    print("PROCEDURE FOR MAKING FRIED RICE AND CHICKEN")
+    print("Step 1: Parboil the chicken with maggi,salt,onions,thyme spice,curry. Then allow the chicken to cook for ten to fifteen minutes,then grill,deep fry or barbecue and reserve the stock water;")
+    print("Step 2: Chop the cabbage,green beans,carrot,liver.Remove the white center seeds from the green pepper and also slice;")
+    print("Step 3: Parboil the rice also and add the meat stock and add water,cook until its about eighty percent done;")
+    print("Step 4: Pour groudnut oil in another dry pot and add sliced onions and stir for about one minute,then add the chopped carrot and green beans and add the cabbage also stir for about one minute then add curry powder then add salt and maggi to taste,then add the almost done rice,stir and cover your pot to cook for five to ten minutes,your fried rice is ready!")
+    print("Step 5: Serve with your chicken.")
+    print("Enjoy your Meal!!!!")
+
+if (menu==2):
+    print("Option 2")
+    print("PROCEDURE FOR MAKING COCONUT RICE AND BEEF")
+    print("Step 1: De-shell and break the coconut,grate the cocunut,the idea is to squeeze out milk from the coconut,add a cup of water and squeeze out milk from which is needed for the preparation of the coconut rice,then filter the squeezed milk;")
+    print("Step 2: Parboil about the rice,grind the tomatoes/peppers.Also parboil the beef with maggi,onion,salt,then fry the beef(optional)")
+    print("Step 3: Pour oil into a dry pot,allow it to heat for about one minute before adding the ground or sliced tomatoes,then fry and stir the tomatoes till it is dried;")
+    print("Step 4: Add the meat extract and the coconut milk also add maggi and salt to taste,allow to boil before adding the rice,cook till it is soft for consumption,your cocnut rice is ready!")
+    print("Step 5: Serve with your beef.")
+    print("Enjoy your Meal!!!!")
+if (menu==3):
+    print("Option 3")
+    print("PROCEDURE FOR MAKING AMALA AND EGUSI SOUP WITH FISH")
+    print("Step 1: Boil the fish and remove them from the stock and place in a different pot or plate;")
+    print("Step 2: Add the ground egusi to the stock and stir.If the stock is not enough to give a medium consistency ,you can add water to get the consistency of evaporated milk or a bit thicker.It will not be as evaporated milk though;")
+    print("Step 3: Cover and cook till the egusi cakes.Stir and add a little bit more water.Watch it closely so that it does not burn;")
+    print("Step 4: After about 25 minutes,you will notice the clear egusi oil coming to the surface of the soup;")
+    print("Step 5: Add the red palm oil and bitter leaves,pepper and salt to taste and cook for about seven minutes.add the cooked fish,if using any other soft vegetable ,add it at this time and stir,leave for about two minutes and then turn off the heat.Your food is ready!")
+    print("Step 6: Boil water and add the yam flour,stir and turn very well,add water and allow to boil for ten to fifteen minutes,turn till the yam flour is smooth and then serve!")
+
+if (menu==4):
+    print("Option 4")
+    print("PROCEDURE FOR MAKING OFADA RICE WITH FISH AND BEEF SAUCE")
+    print("Step 1: Pick out dirt from the ofada rice and parboil the ofada rice,then add more water to boil the ofada rice till it is soft to consume;")
+    print("Step 2: Put a dry pot on fire and heat for about one minute,then add the grounded pepper and stir,then add maggi and salt to taste and the stock of the boiled fish and beef and stir,then add the fish and beef and allow to cook for ten minutes,your food is ready!")
+    print("Step 3: Serve with food!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
