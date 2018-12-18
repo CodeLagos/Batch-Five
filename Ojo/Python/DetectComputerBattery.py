e7ff800c2742acf5406b78bf8b9c0e52730706af
voatilola <45639525+voatilola@users.noreply.github.com>
2018-12-18 22:32:38 +0100

    Create DetectComputerBattery.py

---
+++ Ojo/Python/DetectComputerBattery.py
@@ -0,0 +1,29 @@
+
+
+import ctypes
+from ctypes import wintypes
+
+class SYSTEM_POWER_STATUS(ctypes.Structure):
+    _fields_ = [
+        ('ACLineStatus', wintypes.BYTE),
+        ('BatteryFlag', wintypes.BYTE),
+        ('BatteryLifePercent', wintypes.BYTE),
+        ('Reserved1', wintypes.BYTE),
+        ('BatteryLifeTime', wintypes.DWORD),
+        ('BatteryFullLifeTime', wintypes.DWORD),
+    ]
+
+SYSTEM_POWER_STATUS_P = ctypes.POINTER(SYSTEM_POWER_STATUS)
+
+GetSystemPowerStatus = ctypes.windll.kernel32.GetSystemPowerStatus
+GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
+GetSystemPowerStatus.restype = wintypes.BOOL
+
+status = SYSTEM_POWER_STATUS()
+if not GetSystemPowerStatus(ctypes.pointer(status)):
+    raise ctypes.WinError()
+print 'ACLineStatus', status.ACLineStatus
+print 'BatteryFlag', status.BatteryFlag
+print 'BatteryLifePercent', status.BatteryLifePercent
+print 'BatteryLifeTime', status.BatteryLifeTime
+print 'BatteryFullLifeTime', status.BatteryFullLifeTime
