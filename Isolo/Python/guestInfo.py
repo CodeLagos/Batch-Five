4d09029d482f98954fe083e2325a05b96905b60f
Ayebo Orimisan <43127774+ayebooriz@users.noreply.github.com>
2018-12-18 16:02:49 +0100

    Create guestInfo.py

---
+++ Isolo/Python/guestInfo.py
@@ -0,0 +1,29 @@
+#davidakwe's project
+#firsttimers visit card
+#08125745139
+#codelagosbatch5
+
+ 
+print("GUEST INFORMATION CARD")
+print("With great pleasure we warmly welcome you to covenant christain centre and belive that without a doubt you received pratical wisdom through the word of faith today.")
+print("please kindly fill this visitors form and return to the welcoming unit")
+date=input(" Date:")
+name=input("Name:")
+input("Address:")
+input("Telephone Number:")
+input("Email Address:")
+print("PLEASE fill ALL THAT APPLIES TO YOUR STATUS")
+input("Gender:")
+input("Marital status:")
+input("Age:")
+input("will you visit again:")
+print("will you like to join our youth church or rather get a fellowship closest to you:")
+input("reply here:")
+input("would you liked to be visited at your contact address:")
+input("what time is ideal to visit you:")
+input("prayer request:")
+print("for  more information kindly visit us  www.insightforliving.org  to access our online messages,mails,and publications")
+ 
+
+
+print("                                THANKS FOR COMING!!!!!!   ")
