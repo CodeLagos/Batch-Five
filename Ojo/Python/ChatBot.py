a50eae5a57cd69518c3a6613e1650f085dc88667
voatilola <45639525+voatilola@users.noreply.github.com>
2018-12-18 22:49:21 +0100

    Create ChatBot.py

---
+++ Ojo/Python/ChatBot.py
@@ -0,0 +1,85 @@
+import time
+import random
+print("MY NAME IS ROB, NICE  HAVING YOU HERE. ")
+time.sleep(4)
+name = input("Hello, what is your name? ")
+time.sleep(3)
+print("Hello " + name)
+time.sleep(3)
+feeling = input("How are you today? ")
+if "good" in feeling:
+    time.sleep(3)
+    print("I'm feeling good too!")
+else:
+    print("Good to know that!")
+time.sleep(3)
+favcolour = input("What is your favourite colour? ")
+colours = ["red", "green","blue", "black","white","yellow","pink" ]
+time.sleep(3)
+print("My favourite colour is " + random.choice(colours))
+time.sleep(3)
+Age=input("How old are you?")
+time.sleep(3)
+if "20" in Age:
+    print("I'm older than you are.")
+else:
+    print("I'm younger than you are.")
+time.sleep(3)
+favsubject=input("What is your favourite subject? ")
+time.sleep(3)
+subjects=["English Language", "Mathematics", "Geography","Biology", "Physics", "Chemistry"]
+time.sleep(3)
+print("My favourite subject is " + random.choice(subjects))
+time.sleep(3)
+President=input("Who is the president of your country?")
+if "Buhari" in President:
+ time.sleep(3)
+ print("My president is Donald Trump.")
+else:
+    print("Ok")
+time.sleep(3)
+country=input("What is your country of origin?")
+time.sleep(3)
+if "Nigeria" in country:
+    time.sleep(3)
+    print("I'm from the United States of America")
+else:
+    print("Good to know that.")
+time.sleep(3)
+Dialect=input("Do you speak English?")
+if "yes" in Dialect:
+ time.sleep(3)
+ print("Me too.")
+else:
+    print("I speak English and Machine languages respectively.")
+time.sleep(3)
+Eat=input("Have you had something to eat today?")
+time.sleep(3)
+if "yes" in Eat:
+      print("That's great.")
+else:
+      print("Oh my goodness!")
+time.sleep(3)
+State=input("How many states are there in Nigeria?")
+if "36" in State:
+    
+        print("ok.")
+else:
+        print("Are you sure?")
+time.sleep(3)
+Leaving=input("Feeling very hungry, I have to go please.")
+if "ok" in Leaving:
+ time.sleep(3)
+print("Nice chatting with you.")
+
+            
+    
+                
+        
+        
+                      
+      
+            
+            
+    
+            
