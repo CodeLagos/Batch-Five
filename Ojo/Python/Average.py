ae8239173222c9afb2194c81a91236e9d3375993
voatilola <45639525+voatilola@users.noreply.github.com>
2018-12-18 22:56:12 +0100

    Create Average.py

---
+++ Ojo/Python/Average.py
@@ -0,0 +1,38 @@
+#print out the purpose of the program
+print("this is a program to determine the mean average of ten numbers")
+
+#print out the instructions
+print("this program will only work for whole numbers")
+
+#ask the user to enter the first number
+first_number = int(input("please enter the first number:"))
+
+#ask the user to enter the second number
+second_number = int(input("please enter the second number:"))
+
+#ask the user to enter the third number
+bthird_number = int(input ("please enter the third number:"))
+
+#ask the user to enter the fourth number
+fourth_number = int(input ("please enter the fourth number:"))
+
+#ask the user to enter the fifth number
+fifth_number = int(input ("please enter the fifth number:"))
+
+#ask the user to enter the sixth number
+sixth_number = int(input ("please enter the sixth number:"))
+
+#ask the user to enter the seventh number
+seventh_number = int(input ("please enter the seventh number:"))
+
+#ask the user to enter the eighth number
+eight_number = int(input ("please enter the eighth number:"))
+
+#ask the user to enter the nineth number
+nineth_number = int(input ("please enter the nineth number:"))
+
+#ask the user to enter the tenth number
+tenth_number = int(input("please enter the tenth number:"))
+
+mean = (first_number+second_number+third_number+fourth_number+fifth_number+sixth_number+seventh_number+eight_number+nineth_number+tenth_number)/10
+print(round(mean,1))
