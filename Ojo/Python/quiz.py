a69b849d314daa9c45fb1d9b0310b66c2e5b09c2
voatilola <45639525+voatilola@users.noreply.github.com>
2018-12-18 22:47:45 +0100

    Create quiz.py

---
+++ Ojo/Python/quiz.py
@@ -0,0 +1,61 @@
+ #Quiz created by ADAMSON SAHEED
+print ("WELCOME TO ADAMSON'S QUIZ GENERATOR")
+print("THIS QUIZ IS DESIGNED TO TEST YOUR GENERAL KNOWLEDGE")
+print("PLEASE SELECT THE CORRECT ANSWER FROM THE FOLLOWING OPTIONS")
+print("ALSO NOTE THAT THIS QUIZ WILL BE GRADED")
+
+score = 0
+
+question1 = input("Who is the first prime minister of Nigeria?\n(a)sir tafawa balewa\n(b)Nnamdi azikiwe\n(c)Olusegun obasanjo\n(d)Sanni abacha\n")
+if(question1.lower()=="a"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+question2 = input("Who is the current lagos state governor?\n(a)Bola ahmed tinubu\n(b)Akinwunmi ambode\n(c)Olusegun obasanjo\n(d)Sanni abacha\n")
+if(question2.lower()=="b"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+question3 = input("Who is the current president of united state of america?\n(a)sir tafawa balewa\n(b)Nnamdi azikiwe\n(c)Donald trump\n(d)Sanni abacha\n")
+if(question3.lower()=="c"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+question4 = input("Who is the current president of nigeria?\n(a)sir tafawa balewa\n(b)muhammed buhari\n(c)Olusegun obasanjo\n(d)Sanni abacha\n")
+if(question4.lower()=="b"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+
+        print("Wrong, KEEP TRYING!")
+question5 = input("When did nigeria got their own independence?\n(a)1960\n(b)1963\n(c)1968\n(d)1945\n")
+if(question5.lower()=="a"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+question6 = input("who define politics as man a is a political animal ?\n(a)plato\n(b)socrate\n(c)thomas hobbes \n(d)Aristotle\n")
+if(question6.lower()=="d"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+question7 = input("Who is the first former president of United state of America ?\n(a)Donald trump\n(b)Barrack obama\n(c)Olusegun obasanjo\n(d)Sanni abacha\n")
+if(question7.lower()=="b"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+question8 = input("Who defines democracy as the government of the people by the people and for the people ?\n(a)wilson churchwill\n(b)Auguste comte\n(c)abraham lincoln\n(d)Sanni abacha\n")
+if(question8.lower()=="c"):
+    print("Correct, GOOD JOB!")
+    score = score + 1
+else:
+        print("Wrong, KEEP TRYING!")
+sum = score + 1
+print("QUIZ ENDED, SEE TOTAL SCORE BELOW")
+print("Total score is" , score)
