134d90163e3cedd25b8789e1a31c6a8ed48d4f19
SaaduRasheed <45602793+SaaduRasheed@users.noreply.github.com>
2018-12-18 13:14:35 +0100

    Add files via upload

---
+++ Ikorodu/Java/Adebayo Bude Project/month of the year/src/month/of/the/year/MonthOfTheYear.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package month.of.the.year;
+
+/**
+ *
+ * @author Bayo Bude
+ */
+public class MonthOfTheYear {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int mnth = 7;
+  switch (mnth) {
+   case 1:
+    System.out.println("Showing Month: January");
+    break;
+   case 2:
+    System.out.println("Showing Month: February");
+    break;
+   case 3:
+    System.out.println("Showing Month: March");
+    break;
+   case 4:
+    System.out.println("Showing Month: April");
+    break;
+   case 5:
+    System.out.println("Showing Month: May");
+    break;
+   case 6:
+    System.out.println("Showing Month: June");
+    break;
+   case 7:
+    System.out.println("Showing Month: July");
+    break;
+   case 8:
+    System.out.println("Showing Month: August");
+    break;
+   case 9:
+    System.out.println("Showing Month: September");
+    break;
+   case 10:
+    System.out.println("Showing Month: October");
+    break;
+   case 11:
+    System.out.println("Showing Month: November");
+    break;
+   case 12:
+    System.out.println("Showing Month: December");
+    break;
+   default:
+    System.out.println("Invalid input - Wrong month number.");
+    break;
+  }
+ }
+}
+    
+    
+
