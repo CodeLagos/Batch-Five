857ddab04c5fdfcc039f9a7fdf81e655ac52df3d
Uche Ifunanya Okeke <43139388+UcWealth@users.noreply.github.com>
2018-12-20 15:01:41 +0100

    Add files via upload

---
+++ ROPHEKA, MUSHIN/PYTHON/Jamb_questionnaire.py
@@ -0,0 +1,141 @@
+#JAMB QUESTIONNAIRE 2017
+#AUTHOR: EZE EBUKA EVARISTUS
+
+print("jamb 2017 past questions and answers")
+print("please,you are to choose only one answer from the options given to you")
+question1 = print("which of the following is not a characteristic of parliamentary system of government?,")
+answer = input("1. ministers are usually members of parliament \n2. the head of state is the powerful organ of government \n3. the prime minister is politically responsible to parliament")
+if answer == "2":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+
+
+
+
+question2 = print("capitalism is an economic system in which?,")
+answer = input("1. the economy of the state is centrally planned \n2. private persons are permitted yo undertake enterprises \n3. accumulation of private property is forbidden \n4. that means of production are owned and controlled by the state \n5. all big industries and the land are publicly owned for common good")
+if answer == "2":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+
+
+
+
+question3 = print("the process of defying persons of the right of voting is called?,")
+answer = input("1. enfranchisement \n2. disqualification \n3. dismissal \n4. prohibition \n5. dis-enfranchisement")
+if answer == "5":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+
+
+
+question4 = print("bicameralism refers to?,")
+answer = input("1. A one chamber legislature \n2. The process of voting in the legislature \n3. The upper chamber in a legislature \n4. A two chamber legislature \n5. Legislature in all sovereign states")
+if answer == "4":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+
+
+
+
+question5 = print("the principle of seperation of powers implies that the three arms of government work?,")
+answer = input("1. Seperately \n2.Independently and cooperatively \n3. Against one another \n4. Reluctantly and gradually for the executive \n5. Together in the interest of other nations")
+if answer == "2":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+
+
+
+
+question6 = print("the main function of the judiciary is?,")
+answer = input("1. Serve as the watchdog \n2. Enact laws \n3. Execute the laws of the land \n4. Interpret the law \n5. Protect the interest of accused persons")
+if answer == "4":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+
+
+
+
+question7 = print("an unwritten constitution operates in?,")
+answer = input("1. Guinea \n2. USA \n3. Great Britain \n4. China \n5. Nigeria")
+if answer == "3":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+   
+   
+   
+question8 = print("the primary function of a legislature is?")
+answer = input("1. appointing a president \n2. law  making \n3. vetoing bills \n4. monitoring the judiciary \n5. reassigning civil servants")
+if answer == "2":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+   
+   
+   
+question9 = print("according to marxist theory, those who own and control the economy in a capitalist society are?")
+answer = input("1. exploiters \n2. colonialist \n3. workers \n4. shareholders \n5. bourgeosie")
+if answer == "5":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+   
+question10 = print("when the electorate vote for representatives who in turn vote on their behalf we say it is?")
+answer = input("1. an indirect election \n2. an unfair election \n3. a rigged election \n4. a disputed  election \n5. a biased election")
+if answer == "1":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+    
+    
+    
+question11 = print("an election whichis conducted to fill a seat in legislature is called?")
+answer =input("1. bye election \n2. general election \n3. referendum \n4. plebiscite \n5. mini election")
+if answer == "1":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+   
+
+question12 = print("which of the following countries does not operate a federal constitution?")
+answer = input("1. USA \n2. Nigeria \n3. canada \n4. france \n5. switzerland")
+if answer ==  "4":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+   
+   
+question13 = print("the principle of anonymity of civil servants states that they?")
+answer = input(" 1.they have a career \n2. they are not the servants of a particular government \n3. they are trained for the duties they perform \n4.  they are credited and blamed for anything they do \n5. they are entitled to pension and gratuity when they retire")
+if answer == "3":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+
+question14 = print("a popular principle of colonial administration is?")
+answer = input("1. assimilation \n2. colonisation \n3. indirect rule \n4. westernisation \n5. patternalism")
+if answer == "3":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
+   
+   
+question15 = print("one feature of a totalitarian state is the existence of?")
+answer = input("1. a single recognised party \n2. pressure groups \n3. opposition groups \n4. a colonial power \n5. fierce political rivalry.")
+if answer == "1":
+   print("your answer is correct")
+else:
+   print("your answer is wrong")
