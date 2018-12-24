bed55ce8e46a8eb29413bec4b93ca607bff7b900
tola-impactlogistics <30537616+tola-impactlogistics@users.noreply.github.com>
2018-12-24 10:31:36 +0100

    Add files via upload

---
+++ Old Secretariat Library Ikeja/Java/fproject/cosine.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fproject;
+    //private float angle;
+    //public void setAngle(float angle){
+        //this.angle=angle;
+    //}
+    //public float getAngle(){
+        //return this.angle;
+    //}
+    //public float sine(){
+        //float angle=
+        //return 0;
+    //}
+    //public float cosine(){
+        //return 0;
+    //}
+    //public float tan(){
+        //return 0;
+    //}
+public class cosine{
+   public static void main(String[] args){
+        double a=45;
+        double b=100;
+        a=Math.toRadians(a);
+        b=Math.toRadians(b);
+        System.out.println("Math.sin(" + a + ")=" + Math.cos(a)); 
+        System.out.println("Math.sin(" + b + ")=" + Math.cos(b)); 
+   }
+}
