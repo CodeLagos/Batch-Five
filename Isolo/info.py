934b9405ba1141468859858c42d7db01f5029c95
Abdullahi Abdulkabir <33360580+AbdullahiAbdulkabir@users.noreply.github.com>
2019-10-03 08:56:27 +0100

    Update info.py

--- Isolo/info.py
+++ Isolo/info.py
@@ -1,1 +1,1 @@
-print("This Folder contains the students projects for Batch5 Isolo center\n Thank you for checking on us")
+#print("Those Folder contains the students projects for Batch5 Isolo center\n Thank you for checking on us")
